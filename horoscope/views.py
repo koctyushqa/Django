dcc721242b33f319781ebffba4c35dbb618ad513
Konstantin <kovucholegich@mail.ru>
2022-01-19 23:53:25 +0300

    Django_project

--- horoscope/views.py
+++ horoscope/views.py
@@ -78,3 +78,37 @@ def get_info_about_elements(request, sign_type: str):
             redirect_path_zodiacs = reverse("horoscope-name", args=[i])
             result += f"<li> <a href={redirect_path_zodiacs}> {i.title()} </a> </li>"
     return HttpResponse(f'<ul> {result} </ul>')
+
+
+def determine_sign_by_date(request, month: int, day: int):
+    result = HttpResponseNotFound("Некорректная дата.")
+    if month > 12 or day > 31:
+        return result
+    else:
+        if (month == 3 and day >= 21) or (month == 4 and day <= 20):
+            result = 'aries'
+        elif (month == 4 and day >= 21) or (month == 5 and day <= 21):
+            result = 'taurus'
+        elif (month == 5 and day >= 22) or (month == 6 and day <= 21):
+            result = 'gemini'
+        elif (month == 6 and day >= 22) or (month == 7 and day <= 22):
+            result = 'cancer'
+        elif (month == 7 and day >= 23) or (month == 8 and day <= 23):
+            result = 'leo'
+        elif (month == 8 and day >= 24) or (month == 9 and day <= 23):
+            result = 'virgo'
+        elif (month == 9 and day >= 24) or (month == 10 and day <= 23):
+            result = 'libra'
+        elif (month == 10 and day >= 24) or (month == 11 and day <= 22):
+            result = 'scorpio'
+        elif (month == 11 and day >= 23) or (month == 12 and day <= 21):
+            result = 'sagittarius'
+        elif (month == 12 and day >= 22) or (month == 1 and day <= 20):
+            result = 'capricorn'
+        elif (month == 1 and day >= 21) or (month == 2 and day <= 18):
+            result = 'aquarius'
+        elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+            result = 'pisces'
+
+        url_sign = reverse('horoscope-name', args=[result])
+        return HttpResponse(f'Месяц: {month}. День: {day}. Соответствует знаку Зодиака - <a href={url_sign}>{result}</a>.')
