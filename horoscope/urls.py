28bcd897cc144a6845fe8d94d2c4a88d9c536a69
Konstantin <kovucholegich@mail.ru>
2022-01-23 14:39:07 +0300

    Django_project

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="horoscope-index"),
     path('type/', views.get_info_about_type),
     path('<int:month>/<int:day>/', views.determine_sign_by_date),
     path('type/<str:sign_type>/', views.get_info_about_elements, name="elements-name"),
